55c2712daf0085a42da603915efde9e122c5cfc7
Zoki Naina <admin@aikcraft.com>
2026-08-15 10:10:08 +0300

    logo

--- src/app/(full-width-pages)/(auth)/layout.tsx
+++ src/app/(full-width-pages)/(auth)/layout.tsx
@@ -25,12 +25,12 @@ export default function AuthLayout({
                   <Image
                     width={231}
                     height={48}
-                    src="./images/logo/auth-logo.svg"
+                    src="/images/logo/auth.png"
                     alt="Logo"
                   />
                 </Link>
                 <p className="text-center text-gray-400 dark:text-white/60">
-                  Free and Open-Source Tailwind CSS Admin Dashboard Template
+                  The intelligent virtual staff your club could never afford.
                 </p>
               </div>
             </div>
